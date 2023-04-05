c2d8edaa8ea8f866e56adf9a97433d8f10d97230
Elena Makaeva <yamaslena@yandex.ru>
2023-04-06 00:18:47 +0700

    feat: add pets pagination

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -1,108 +1,37 @@
 import { pets } from './data.js';
-import { MainPetCard } from './petCard.js';
+
+import {
+  moveLeft,
+  moveRight,
+  caruselHandler,
+  BTN_LEFT,
+  BTN_RIGHT,
+  renderBlock,
+  renderMainPetsCards,
+} from './utils.js';
 // const mediaDesktop = window.matchMedia('(min-width: 769px)');
 // const mediaTablet = window.matchMedia('(max-width: 768px)').matches;
 // const mediaMobile = window.matchMedia('(max-width: 320px)').matches;
 const mainCaruselWrapper = document.querySelector('.main .carusel');
 const petsCaruselWrapper = document.querySelector('.our-pets .carusel');
 
-const BTN_LEFT = document.querySelector('#btn-left');
-const BTN_RIGHT = document.querySelector('#btn-right');
-const CAROUSEL = document.querySelector('#carusel');
+const PETS_CAROUSEL = document.querySelector('#pets-carusel');
+const DOUBLE_ROW_LEFT = document.querySelector('#double-row-left');
+const ROW_LEFT = document.querySelector('#row-left');
+const ROW_RIGHT = document.querySelector('#row-right');
+const DOUBLE_ROW_RIGHT = document.querySelector('#double-row-right');
+const PAGE = document.querySelector('#number h4');
+
 let left;
 let center;
 let right;
-
-const shuffledArr = (arr) => arr.sort(() => Math.random() - 0.5);
-
-function compare(arr1, arr2) {
-  // console.log('arr1', arr1);
-  // console.log('arr2', arr2);
-
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] === arr2[j]) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-const renderMainPetsCards = function (data) {
-  const mainPetsCards = [];
-  data.map((el) => {
-    mainPetsCards.push(new MainPetCard(el));
-  });
-  return mainPetsCards;
-};
-
-let prevArr = [];
-const renderBlock = (data, num, position) => {
-  let newArr = [];
-  // console.log('prevArr', prevArr);
-  let arr = shuffledArr(data).slice(0, num);
-  // console.log(arr);
-  arr.map((el) => newArr.push(el.id));
-
-  if (!compare(newArr, prevArr)) {
-    let newPets = renderMainPetsCards(arr);
-    newPets.map((el) => position.append(el.createMainPetCard()));
-    prevArr = newArr;
-  } else {
-    renderBlock(data, num, position);
-  }
-};
-
-const moveLeft = () => {
-  CAROUSEL.classList.add('transition-left');
-  BTN_LEFT.removeEventListener('click', moveLeft);
-  BTN_RIGHT.removeEventListener('click', moveRight);
-};
-
-const moveRight = () => {
-  CAROUSEL.classList.add('transition-right');
-  BTN_LEFT.removeEventListener('click', moveLeft);
-  BTN_RIGHT.removeEventListener('click', moveRight);
-};
-
-const caruselHandler = () => {
-  const ITEM_LEFT = document.querySelector('.item-left');
-  const ITEM_RIGHT = document.querySelector('.item-right');
-
-  CAROUSEL.addEventListener('animationend', (animationEvent) => {
-    let changedItem;
-    if (animationEvent.animationName === 'move-left') {
-      CAROUSEL.classList.remove('transition-left');
-      changedItem = ITEM_LEFT;
-
-      document.querySelector('.item-center').innerHTML = ITEM_LEFT.innerHTML;
-    } else {
-      CAROUSEL.classList.remove('transition-right');
-      changedItem = ITEM_RIGHT;
-
-      document.querySelector('.item-center').innerHTML = ITEM_RIGHT.innerHTML;
-    }
-
-    changedItem.innerHTML = '';
-
-    renderBlock(pets, 3, changedItem);
-
-    // !нужно запомнить предыдущую карточку левую и правую
-    // !проверить, если она есть, то применять ее
-
-    // let newPets = renderMainPetsCards(shuffledArr(pets)).slice(0, 3);
-    // newPets.map((el) => changedItem.append(el.createMainPetCard()));
-
-    BTN_LEFT.addEventListener('click', moveLeft);
-    BTN_RIGHT.addEventListener('click', moveRight);
-  });
-};
 
 window.onload = function () {
   if (pets) {
     if (mainCaruselWrapper) {
       // mainCaruselWrapper.innerHTML = '';
+
+      let mainPets = pets.slice(0, 7);
       left = document.createElement('div');
       left.classList.add('item-left', 'columns');
       center = document.createElement('div');
@@ -113,39 +42,83 @@ window.onload = function () {
       BTN_LEFT.addEventListener('click', moveLeft);
       BTN_RIGHT.addEventListener('click', moveRight);
 
-      renderBlock(pets, 3, left);
-      renderBlock(pets, 3, center);
-      renderBlock(pets, 3, right);
+      let number;
+      if (document.documentElement.clientWidth <= 320) {
+        number = 1;
+      } else if (document.documentElement.clientWidth <= 768) {
+        number = 2;
+      } else number = 3;
+
+      renderBlock(mainPets, number, left);
+      renderBlock(mainPets, number, center);
+      renderBlock(mainPets, number, right);
       mainCaruselWrapper.append(left);
       mainCaruselWrapper.append(center);
       mainCaruselWrapper.append(right);
-      caruselHandler();
+      caruselHandler(mainPets, number);
     }
 
     if (petsCaruselWrapper) {
       // petsCaruselWrapper.innerHTML = '';
-      const left = document.createElement('div');
-      left.classList.add('item-left', 'columns');
+      center = document.createElement('div');
+      center.classList.add('item-center', 'columns');
 
       let number;
-
       if (document.documentElement.clientWidth <= 320) {
         number = 3;
       } else if (document.documentElement.clientWidth <= 768) {
         number = 6;
       } else number = 8;
 
-      renderMainPetsCards(pets)
-        .slice(0, number)
-        .map((el) => left.append(el.createMainPetCard()));
+      renderBlock(pets, number, center);
 
-      petsCaruselWrapper.append(left);
+      petsCaruselWrapper.append(center);
+
+      const ITEM_CENTER = document.querySelector('.item-center');
+
+      let numberPage = 1;
+
+      const onRight = () => {
+        let start = numberPage * number;
+        let end = start + number;
+
+        if (end <= pets.length) {
+          let petsOnPage = pets.slice(start, end);
+          ITEM_CENTER.innerHTML = '';
+
+          renderMainPetsCards(petsOnPage).map((el) => ITEM_CENTER.append(el.createMainPetCard()));
+
+          numberPage++;
+          PAGE.innerHTML = numberPage;
+          ROW_LEFT.classList.add('active');
+          ROW_LEFT.classList.remove('disabled-link');
+        } else {
+          ROW_RIGHT.classList.add('disabled-link');
+          ROW_RIGHT.classList.remove('active');
+        }
+      };
+      ROW_RIGHT.addEventListener('click', onRight);
+
+      const onLeft = () => {
+        let end = (numberPage - 1) * number;
+        let start = end - number;
+
+        if (start <= pets.length && numberPage > 1) {
+          ROW_LEFT.classList.add('active');
+          ROW_LEFT.classList.remove('disabled-link');
+          let petsOnPage = pets.slice(start, end);
+          ITEM_CENTER.innerHTML = '';
+
+          renderMainPetsCards(petsOnPage).map((el) => ITEM_CENTER.append(el.createMainPetCard()));
+
+          numberPage--;
+          PAGE.innerHTML = numberPage;
+        } else {
+          ROW_LEFT.classList.add('disabled-link');
+          ROW_LEFT.classList.remove('active');
+        }
+      };
+      ROW_LEFT.addEventListener('click', onLeft);
     }
-
-    // addTestimonialsHandler();
   }
 };
-
-// console.log(
-//   'Оценка за задание: 100/100 баллов\n\nТребования к вёрстке\n\nВёрстка страницы Main соответствует макету при ширине экрана 1280px: +14\n\nблок <header>: +2\nблок Not only: +2\nблок About: +2\nблок Our Friends: +2\nблок Help: +2\nблок In addition: +2\nблок <footer>: +2\nВёрстка страницы Main соответствует макету при ширине экрана 768px: +14\n\nблок <header>: +2\nблок Not only: +2\nблок About: +2\nблок Our Friends: +2\nблок Help: +2\nблок In addition: +2\nблок <footer>: +2\nВёрстка страницы Main соответствует макету при ширине экрана 320px: +14\n\nблок <header>: +2\nблок Not only: +2\nблок About: +2\nблок Our Friends: +2\nблок Help: +2\nблок In addition: +2\nблок <footer>: +2\nВёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6\nблок <header>: +2\nблок Our Friends: +2\nблок <footer>: +2\nВёрстка страницы Pets соответствует макету при ширине экрана 768px: +6\n\nблок <header>: +2\nблок Our Friends: +2\nблок <footer>: +2\nВёрстка страницы Pets соответствует макету при ширине экрана 320px: +6\n\nблок <header>: +2\nблок Our Friends: +2\nблок <footer>: +2\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +20\n\nнет полосы прокрутки при ширине страницы Main от 1280рх до 768рх: +5\nнет полосы прокрутки при ширине страницы Main от 768рх до 320рх: +5\nнет полосы прокрутки при ширине страницы Pets от 1280рх до 768рх: +5\nнет полосы прокрутки при ширине страницы Pets от 768рх до 320рх: +5\nВерстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции (Примеры неправильной и правильной реализации): +8\n\nна странице Main: +4\nна странице Pets: +4\nПри ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню: +4\nОткрытие меню при клике на иконку бургер-меню на текущем этапе не проверяется\nВерстка обеих страниц валидная: для проверки валидности вёрстки используйте сервис https://validator.w3.org/ : +8\n',
-// );

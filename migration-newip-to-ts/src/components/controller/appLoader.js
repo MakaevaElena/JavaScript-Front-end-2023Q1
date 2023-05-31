725e2c3b1e922f747cdcbfff0d3e33b03af2b024
Elena Makaeva <yamaslena@yandex.ru>
2023-05-31 16:49:43 +0700

    feat: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'f4e7ac2d5bec4aa3a63a49c9bb3a9ea3', // получите свой ключ https://newsapi.org/
         });
     }
 }

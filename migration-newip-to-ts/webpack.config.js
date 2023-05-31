a06fe6fdc81abb3bb92e4c8ad5cc5d3c1a602b65
Elena Makaeva <yamaslena@yandex.ru>
2023-05-31 18:25:28 +0700

    feat: configure webpack

--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin'); //чтобы eslint работал во время работы Webpack
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    // entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'), //чтобы при замене .js на .ts он его тоже подхватил
     mode: 'development',
     module: {
         rules: [
@@ -12,10 +14,11 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            { test: /\.ts$/i, use: 'ts-loader' }, //чтобы Wepback обрабатывал .ts-файлы
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'], //это определяет порядок поиска файлов в импортах
     },
     output: {
         filename: 'index.js',
@@ -27,6 +30,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }), //в настройках указываем тип файлов .ts
     ],
 };
 

7c01d7bd9f299a7136269cc802f4ab6e5ccfaf1d
Elena Makaeva <yamaslena@yandex.ru>
2023-06-01 11:49:29 +0700

    fix: news.ts typing

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -16,25 +16,26 @@ class News {
             // if (!newsClone) throw new Error('error');
             if (idx % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
 
-            newsClone.querySelector<HTMLElement>('.news__meta-photo')!.style.backgroundImage = `url(${
+            (newsClone.querySelector('.news__meta-photo') as HTMLDivElement).style.backgroundImage = `url(${
                 item.urlToImage || 'img/news_placeholder.jpg'
             })`;
-            newsClone.querySelector('.news__meta-author')!.textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date')!.textContent = item.publishedAt
+            (newsClone.querySelector('.news__meta-author') as HTMLLIElement).textContent =
+                item.author || item.source.name;
+            (newsClone.querySelector('.news__meta-date') as HTMLLIElement).textContent = item.publishedAt
                 .slice(0, 10)
                 .split('-')
                 .reverse()
                 .join('-');
 
-            newsClone.querySelector('.news__description-title')!.textContent = item.title;
-            newsClone.querySelector('.news__description-source')!.textContent = item.source.name;
-            newsClone.querySelector('.news__description-content')!.textContent = item.description;
+            (newsClone.querySelector('.news__description-title') as HTMLElement).textContent = item.title;
+            (newsClone.querySelector('.news__description-source') as HTMLElement).textContent = item.source.name;
+            (newsClone.querySelector('.news__description-content') as HTMLElement).textContent = item.description;
             newsClone.querySelector('.news__read-more a')?.setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
 
-        document.querySelector('.news')!.innerHTML = '';
+        (document.querySelector('.news') as HTMLElement).innerHTML = '';
         document.querySelector('.news')?.appendChild(fragment);
     }
 }

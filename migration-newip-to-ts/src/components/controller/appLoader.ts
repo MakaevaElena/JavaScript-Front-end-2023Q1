94b84b9a1d36ee38acbc7c36f216c7c45f201a42
Elena Makaeva <yamaslena@yandex.ru>
2023-06-11 14:00:18 +0700

    fix: change proxy server

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -5,12 +5,12 @@ class AppLoader extends Loader {
         // super('https://newsapi.org/v2/', {
         //     apiKey: 'f4e7ac2d5bec4aa3a63a49c9bb3a9ea3', // получите свой ключ https://newsapi.org/
         // });
-        // super('https://rss-news-api.onrender.com/', {
-        //     apiKey: 'f4e7ac2d5bec4aa3a63a49c9bb3a9ea3',
-        // });
-        super('http://127.0.0.1:8075/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: 'f4e7ac2d5bec4aa3a63a49c9bb3a9ea3',
         });
+        // super('http://127.0.0.1:8075/', {
+        //     apiKey: 'f4e7ac2d5bec4aa3a63a49c9bb3a9ea3',
+        // });
     }
 }
 

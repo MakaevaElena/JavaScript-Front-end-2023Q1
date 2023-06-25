5d5164fb16f7ed92d0e7b49576822542dd482ce5
Elena Makaeva <yamaslena@yandex.ru>
2023-06-25 10:56:17 +0700

    fix: change configs, still error

--- rs-css/webpack.config.js
+++ rs-css/webpack.config.js
@@ -1,48 +1,48 @@
 // https://github.com/Jeneko/News-api-migration-walkthrough/blob/main/README.md
-const path = require('path');
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const EslingPlugin = require('eslint-webpack-plugin'); //чтобы eslint работал во время работы Webpack
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const EslingPlugin = require("eslint-webpack-plugin"); //чтобы eslint работал во время работы Webpack
 
 const baseConfig = {
-  entry: path.resolve(__dirname, './src/index'), //чтобы при замене .js на .ts он его тоже подхватил
-  mode: 'development',
+  entry: path.resolve(__dirname, "./src/index"), //чтобы при замене .js на .ts он его тоже подхватил
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
-      { test: /\.ts$/i, use: 'ts-loader' }, //чтобы Wepback обрабатывал .ts-файлы
+      { test: /\.ts$/i, use: "ts-loader" }, //чтобы Wepback обрабатывал .ts-файлы
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'], //это определяет порядок поиска файлов в импортах
+    extensions: [".ts", ".js"], //это определяет порядок поиска файлов в импортах
   },
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, '../dist'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "../dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
     }),
     new CleanWebpackPlugin(),
-    new EslingPlugin({ extensions: 'ts' }), //в настройках указываем тип файлов .ts
+    new EslingPlugin({ extensions: "ts" }), //в настройках указываем тип файлов .ts
   ],
 };
 
-module.exports = ({ mode }) => {
-  const isProductionMode = mode === 'prod';
+module.exports = ({}, { mode }) => {
+  const isProductionMode = mode === "production";
   const envConfig = isProductionMode
-    ? require('./webpack.prod.config')
-    : require('./webpack.dev.config');
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
 
   return merge(baseConfig, envConfig);
 };
